bf7934975c6460dab17399219c5722dd48cdd05b
HN TinVN <nguyenpeo120@gmail.com>
2024-07-07 15:46:00 +0700

    Create tinoy.is-an.app.js

---
+++ domains/tinoy.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+    description: 'A website and dashboard for my discord bot', 
+    domain: 'is-an.app', 
+    subdomain: 'tinoy',
+    owner: {
+        email: 'nguyenpeo120@gmail.com',
+    },
+    record: {
+        CNAME: 'tinoy.fly.dev',
+    },
+    proxy: false, 
+
+})
